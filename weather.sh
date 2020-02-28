0b25aa597c59445dbd6ea0a9bf7711187822e196
xiaokang <xiaokang.188@qq.com>
2020-02-28 09:07:44 +0800

    update weather.sh

--- weather.sh
+++ weather.sh
@@ -10,5 +10,5 @@ UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
-  -o result.html \
+  -o weather_forecast.html \
   wttr.in/$CITY?format=3\&$UNIT
