d2cfa0b73b116b0e13dae426ca6a48e61d0ce943
广彐水厂 <57484226+xiaokangxxs@users.noreply.github.com>
2021-05-04 21:29:19 -0500

    Update greetings.sh
    update greetings.sh

--- greetings.sh
+++ greetings.sh
@@ -2,6 +2,13 @@
 
 set -eux
 
+read -p "Timezone (default: Asia/Shanghai): " TZONE
+TZONE=${TZONE:-'Asia/Shanghai'}
+timedatectl set-timezone $TZONE
+
+apt install -y language-pack-zh-hans language-pack-zh-hant
+`which update-locale` LANG=zh_CN.UTF-8
+
 USER=$1
 PARTY=$2
 CORP_ID=$3
