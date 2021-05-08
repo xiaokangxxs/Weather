33d45c6d571a4c83ee94be636f9a2459d63f255c
广彐水厂 <57484226+xiaokangxxs@users.noreply.github.com>
2021-05-08 22:05:54 +0800

    Update greetings.sh
    fix

--- greetings.sh
+++ greetings.sh
@@ -12,4 +12,15 @@ PARTY=$2
 CORP_ID=$3
 CORP_SECRET=$4
 
+CITY=beijing
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o weather_forecast.html \
+  wttr.in/$CITY?format=3\&$UNIT
+
 python MorningGreetings.py --user $USER --party $PARTY --corpid $CORP_ID --corpsecret $CORP_SECRET >> morning_greetings.html
