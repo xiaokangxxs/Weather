9bb4247da7f18efc470d5b7b6f742245ea6130c6
广彐水厂 <57484226+xiaokangxxs@users.noreply.github.com>
2021-09-04 14:19:31 +0800

    Update MorningGreetings.py
    update weather code

--- MorningGreetings.py
+++ MorningGreetings.py
@@ -183,6 +183,38 @@ def get_daily_sentence():
     daily_sentence = content + "\n" + note
     return daily_sentence
 
+def get_weather_today():
+    try:
+        api = 'http://t.weather.itboy.net/api/weather/city/'    #API地址，必须配合城市代码使用
+        city_code = '101011000' #进入https://where.heweather.com/index.html查询你的城市代码
+        tqurl = api + city_code
+        response = requests.get(tqurl)
+        d = response.json()         #将数据以json形式返回，这个d就是返回的json数据
+        if(d['status'] == 200):     #当返回状态码为200，输出天气状况
+            parent = d["cityInfo"]["parent"] #省
+            city = d["cityInfo"]["city"] #市
+            update_time = d["time"] #更新时间
+            date = d["data"]["forecast"][0]["ymd"] #日期
+            week = d["data"]["forecast"][0]["week"] #星期
+            weather_type = d["data"]["forecast"][0]["type"] # 天气
+            wendu_high = d["data"]["forecast"][0]["high"] #最高温度
+            wendu_low = d["data"]["forecast"][0]["low"] #最低温度
+            shidu = d["data"]["shidu"] #湿度
+            pm25 = str(d["data"]["pm25"]) #PM2.5
+            pm10 = str(d["data"]["pm10"]) #PM10
+            quality = d["data"]["quality"] #天气质量
+            fx = d["data"]["forecast"][0]["fx"] #风向
+            fl = d["data"]["forecast"][0]["fl"] #风力
+            ganmao = d["data"]["ganmao"] #感冒指数
+            tips = d["data"]["forecast"][0]["notice"] #温馨提示
+            # 天气提示内容
+            word = "-----------------------------------------" + "\n【今日份天气】\n城市： " + parent + city + \
+                   "\n日期： " + date + " 星期: " + week + "\n天气: " + weather_type + "\n温度: " + wendu_high + " / "+ wendu_low + "\n湿度: " + \
+                    shidu + "\nPM25: " + pm25 + "\nPM10: " + pm10 + "\n空气质量: " + quality + \
+                   "\n风力风向: " + fx + fl + "\n感冒指数: "  + ganmao + "\n温馨提示： " + tips + "\n更新时间: " + update_time
+            return word.encode('utf-8')
+    except:
+        return '【出现错误】\n　　今日天气推送错误，请检查服务或网络状态！'
 
 def greetings():
     hour = int(time.strftime('%H', time.localtime(time.time())))
@@ -196,10 +228,14 @@ def greetings():
         return "认真生活,努力长大-Github Action强力驱动"
 
 
+# def get_sendContent():
+#    sendContent = greetings() + "\n" + get_week_day(datetime.date.today()) + "\n\n" + str(
+#        get_bd_top_list()).replace(
+#        "', '", '\n').replace("['", "").replace("']", "") + "\n\n" + get_daily_sentence() + "\n"
+#    return sendContent
+
 def get_sendContent():
-    sendContent = greetings() + "\n" + get_week_day(datetime.date.today()) + "\n\n" + str(
-        get_bd_top_list()).replace(
-        "', '", '\n').replace("['", "").replace("']", "") + "\n\n" + get_daily_sentence() + "\n"
+    sendContent = greetings() + "\n" + get_weather_today() + "\n"
     return sendContent
 
 
